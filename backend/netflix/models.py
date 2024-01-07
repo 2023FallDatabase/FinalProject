4f9b287ffefb38f8bcac35cf3fdc2253d0f32ba5
Bill Huang <bill93.h@gmail.com>
2024-01-08 04:53:18 +0800

    some big change

--- backend/netflix/models.py
+++ backend/netflix/models.py
@@ -13,7 +13,7 @@ class Country(models.Model):
 class Data(models.Model):
     show_id=models.CharField(max_length=10)
     type=models.CharField(max_length=10)
-    date_added=models.CharField(max_length=15)
+    # date_added=models.CharField(max_length=15) # 2021-02-23
     release_year=models.PositiveSmallIntegerField()
     rating=models.CharField(max_length=15)
     # duration=models.CharField(max_length=15)
@@ -24,8 +24,8 @@ class Director(models.Model):
 
 class Listed(models.Model):
     show_id=models.CharField(max_length=10)
-    listed_in=models.CharField(max_length=40)
-
+    catalog=models.CharField(max_length=40)
+"""
 class Movies(models.Model):
     show_id=models.CharField(max_length=10)
     type=models.CharField(max_length=10)
@@ -33,18 +33,37 @@ class Movies(models.Model):
     release_year=models.PositiveSmallIntegerField()
     rating=models.CharField(max_length=15)
     duration=models.CharField(max_length=15)
-
+"""
 class Netflix(models.Model):
     show_id=models.CharField(max_length=10)
     type=models.CharField(max_length=10)
     title=models.CharField(max_length=50)
     director=models.CharField(max_length=30)
     # we ignore cast-column
-    duration=models.CharField(max_length=15)
-
-class Movies(models.Model):
+    duration=models.CharField(max_length=15, default='0')
+"""
+class Movie(models.Model):
     show_id=models.CharField(max_length=10)
     type=models.CharField(max_length=10)
     date_add=models.CharField(max_length=15)
     release_year=models.PositiveSmallIntegerField()
     rating=models.CharField(max_length=15)
+"""
+class Form(models.Model):
+    show_id=models.CharField(max_length=10, null=False)
+    title=models.CharField(max_length=50, null=False)
+    type=models.CharField(max_length=10, null=False, default='TV show')
+    director=models.CharField(max_length=30,default='None')
+    country=models.CharField(max_length=30, null=False)
+    rating=models.CharField(max_length=15, null=False)
+    duration=models.CharField(max_length=15, null=False) # duration/season
+    release_year=models.PositiveSmallIntegerField(null=False)
+    catalog=models.CharField(max_length=15, null=False)
+    average_score=models.FloatField(null=True)
+    score_count=models.IntegerField(null=True)
+    comments=models.TextField(null=True)
+
+class Comment(models.Model):
+    show_id=models.CharField(max_length=10)
+    comment=models.TextField()
+
