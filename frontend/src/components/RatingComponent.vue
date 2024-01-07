acf739519392b0fef1887c81e7cd460c2b95a9f3
林彥亨 <yanheng0907@gmail.com>
2024-01-08 01:59:58 +0800

    feat rating

---
+++ frontend/src/components/RatingComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="rating-system">
+    <span v-for="star in 5" :key="star" class="star" @click="setRating(star)">
+      {{ star <= rating ? '★' : '☆' }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RatingComponent',
+  props: ['initialRating'],
+  data() {
+    return {
+      rating: this.initialRating
+    };
+  },
+  methods: {
+    setRating(star) {
+      this.rating = star;
+      this.$emit('update-rating', this.rating);
+    }
+  }
+};
+</script>
+
+<style>
+.star {
+  cursor: pointer;
+  color: gold;
+}
+</style>
