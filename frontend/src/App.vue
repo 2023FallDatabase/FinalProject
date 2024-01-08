f5120fe425e4b9f1b85598cc5e59bdfd8c01a30d
林彥亨 <yanheng0907@gmail.com>
2024-01-09 02:43:06 +0800

    feat component and file

---
+++ frontend/src/App.vue
@@ -0,0 +1,87 @@
+<!-- <template>
+  <div id="app">
+    <header>
+      <h1>Rating System</h1>
+    </header>
+
+    <main>
+      <RatingComponent :initialRating="3" @update-rating="handleRatingUpdate"/>
+      <CommentSection :comments="comments" @comment-submitted="addComment"/>
+      <router-view/>
+    </main>
+
+    <footer>
+      <p>&copy; DB_final_project</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import RatingComponent from './components/RatingComponent.vue'; 
+import CommentSection from './components/CommentSection.vue'; 
+export default {
+  name: 'App',
+  components: {
+    RatingComponent,
+    CommentSection
+  },
+  data() {
+    return {
+      comments: [] // Initial comments data
+    };
+  },
+  methods: {
+    handleRatingUpdate(newRating) {
+      // Handle the updated rating
+      console.log('New Rating:', newRating);
+    },
+    addComment(newComment) {
+      this.comments.push(newComment);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 60px;
+}
+
+header {
+  background-color: #a1d573;
+  padding: 15px;
+  font-size: 15px;
+}
+
+footer {
+  background-color: #cde193;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style> -->
+<template>
+  <div id="app">
+    <LoginComponent />
+  </div>
+</template>
+
+<script>
+import LoginComponent from './components/LoginComponent.vue';
+
+export default {
+  components: {
+    LoginComponent
+  }
+};
+</script>
+
+<style>
+/* 這裡你可以添加一些全局樣式 */
+#app {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+// import './assets/main.css'
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import router from './router';
+// import  axios  from  'axios'
+// import  VueAxios  from  'vue-axios'
+// Vue.use(VueAxios, axios)
+// createApp(App).use(router).mount('#app');
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
